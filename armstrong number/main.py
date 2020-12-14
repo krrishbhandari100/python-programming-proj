4803e821120d90ef39c3be1b7ee21523c0909dda
Krrish <krrish@mail2webmaster.com>
2020-12-14 11:06:37 +0530

    initial commit

--- armstrong number/main.py
+++ armstrong number/main.py
@@ -1,26 +1,26 @@
-# num = int(input("Enter a number"))
+num = int(input("Enter a number"))
 
-def func(n):
-    if(n==""):
+def func():
+    if(num==""):
         print("blank not allowed")
     else:
-        n = int(n)
+        num = int(num)
         # Changed num variable to string, 
         # and calculated the length (number of digits)
-        order = len(str(n))
+        order = len(str(num))
 
         # initialize sum
         sum = 0
 
         # find the sum of the cube of each digit
-        temp = n
+        temp = num
         while temp > 0:
             digit = temp % 10
             sum += digit ** order
             temp //= 10
 
         # display the result
-        if n == sum:
-            return True
+        if num == sum:
+            print(num, "is an armstrong number")
         else:
-            return False
+            print(num, "is not an armstrong number")
