feaae43b1a926bb9a11a371ac3571701167c1232
Krrish <krrish@mail2webmaster.com>
2021-01-05 21:00:50 +0530

    initial commit

--- Jarvis AI/main.py
+++ Jarvis AI/main.py
@@ -3,8 +3,17 @@ import speech_recognition as sr
 import datetime
 import armstrong
 import os
+import wikipedia
+import webbrowser
 
-r = sr.Recognizer()
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as m:
+        print("Listening....")
+        uinput = r.listen(m)
+        r.pause_threshold = 1
+        query = r.recognize_google(uinput, language="en-in")
+        return query
 
 def speak(audio):
     engine = pyttsx3.init()
@@ -13,17 +22,28 @@ def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 
+
+def wishme():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning! Sir I am Jarvis how may I help you?")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon! Sir I am Jarvis how may I help you?")
+
+    else:
+        speak("Good Evening! Sir I am Jarvis how may I help you?")
+
+
 if __name__ == "__main__":
-    print("Good Morning Sir, I am Jarvis, How may I help you?")
-    speak("Good Morning Sir, I am Jarvis How may I help you?")
-    print("Listening....")
-    with sr.Microphone() as m:
-        uinput = r.listen(m)
-        query = r.recognize_google(uinput, language="en-in")
-        # print(query)
-    if 1:
-        userinput = query.lower()
+    wishme()
+    
+    jarvistm = True
+    while jarvistm:
+        userinput = takeCommand().lower()
+        # print(userinput)
         if 'date' in userinput:
+            print(userinput)
             print("Sir, The date is ", datetime.datetime.today().strftime('%d'))
             print("The Month is", datetime.datetime.today().strftime('%m'))
             print("and the year is", datetime.datetime.today().strftime('%Y'))
@@ -31,7 +51,9 @@ if __name__ == "__main__":
             speak("Sir, The date is " + datetime.datetime.today().strftime('%d'))
             speak("The Month is " + datetime.datetime.today().strftime('%m'))
             speak("and the year is " + datetime.datetime.today().strftime('%Y'))
+        
         elif 'find armstrong number' in userinput:
+            print(userinput)
             speak("Sir, Please speak the armstrong number")
             # second microphone
             with sr.Microphone() as m1:
@@ -40,6 +62,35 @@ if __name__ == "__main__":
                 query1 = r.recognize_google(uinput1, language="en-in")
                 armstrong.func(int(query1))
         elif 'open code' in userinput:
+            print(userinput)
             print("Opening Visual Studio Code...")
             speak("Opening Visual Studio Code...")
             os.startfile(r"C:\Users\Krrish Bhandari\AppData\Local\Programs\Microsoft VS Code\Code.exe")
+
+        elif 'wikipedia' in userinput:
+            print(userinput)
+            userinput = userinput.replace("wikipedia", "")
+            results = wikipedia.summary(userinput, sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+        
+        elif 'open google' in userinput:
+            print(userinput)
+            speak("Opening Google....")
+            webbrowser.open("https://www.google.com/")
+        
+        elif 'open youtube' in userinput:
+            print(userinput)
+            speak("Opening Youtube....")
+            webbrowser.open("https://www.youtube.com/")
+        
+        elif 'open my online reference' in userinput:
+            print(userinput)
+            speak("Opening your reference....")
+            webbrowser.open("http://krrishbhandari.hyperphp.com/")
+
+        elif 'exit' in userinput:
+            print("bye")
+            speak("Good Bye Sir....")
+            jarvistm = False
